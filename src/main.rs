3c59c3f5bdfea117e3986329002aa4a1d2319f48
Erin Moon <erin@hecke.rs>
2021-03-09 20:45:49 -0600

    wav

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,25 @@
+use core::f32::consts::PI;
+
 use anyhow::Result;
-use itertools::izip;
-use jack::{
-    AudioOut, Client, ClientOptions, Control, NotificationHandler, Port, ProcessHandler,
-    ProcessScope,
-};
-
-struct Ports {
-    x: Port<AudioOut>,
-    y: Port<AudioOut>,
-    z: Port<AudioOut>,
-}
-
-impl Ports {
-    pub fn new(client: &Client) -> Result<Self, jack::Error> {
-        Ok(Self {
-            x: client.register_port("x", AudioOut::default())?,
-            y: client.register_port("y", AudioOut::default())?,
-            z: client.register_port("z", AudioOut::default())?,
-        })
-    }
-}
-
-struct RTProcess {
-    ports: Ports,
-    sample_rate: usize,
-    frame_t: f64,
-    t: f64,
-}
-
-const SCAN_RATE: f64 = 100.; // 100hz => 2pi rad around the cardioid per 1/100 s.
-
-impl ProcessHandler for RTProcess {
-    fn process(&mut self, _: &Client, ps: &ProcessScope) -> Control {
-        let x_out = self.ports.x.as_mut_slice(ps);
-        let y_out = self.ports.y.as_mut_slice(ps);
-        let z_out = self.ports.z.as_mut_slice(ps);
-
-        for (x, y, z) in izip!(x_out.iter_mut(), y_out.iter_mut(), z_out.iter_mut()) {
-            let t = self.t * 2. * std::f64::consts::PI * SCAN_RATE;
-            *x = t.cos() as f32;
-            *y = t.sin() as f32;
-
-            *z = 0.; // we'll just ignore this for now
-
-            self.t += self.frame_t;
-        }
-
-        Control::Continue
-    }
-}
-
-struct NotifProcess {}
-impl NotificationHandler for NotifProcess {
-    fn xrun(&mut self, _client: &Client) -> Control {
-        println!("xrun!");
-
-        Control::Continue
-    }
-}
 
 fn main() -> Result<()> {
-    let (client, _status) = Client::new("scopefig", ClientOptions::NO_START_SERVER)?;
-
-    let process_handler = RTProcess {
-        ports: Ports::new(&client)?,
-        sample_rate: client.sample_rate(),
-        frame_t: 1.0 / client.sample_rate() as f64,
-        t: 0.0f64,
+    let out_spec = hound::WavSpec {
+        channels: 2,
+        sample_rate: 44_100,
+        bits_per_sample: 32,
+        sample_format: hound::SampleFormat::Float,
     };
-    let notif_handler = NotifProcess {};
-    client.activate_async(notif_handler, process_handler)?;
 
-    loop {} // spin
+    let mut writer = hound::WavWriter::create("sin", out_spec)?;
+
+    for t in (0..44100).map(|x| x as f32 / 44100.0) {
+        let y = (t * 440. * 2. * PI).sin();
+        let x = (t * 440. * 2. * PI).cos();
+        writer.write_sample(x)?;
+        writer.write_sample(y)?;
+    }
+
+    writer.finalize()?;
 
     Ok(())
 }
